086a7ae1785538e2b33890ff2820e8f3b5d7c0ad
recofka <recofka@gmail.com>
2020-06-02 21:44:34 +0200

    Inserted items in the table items, created the items route

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,29 @@
 import express from 'express';
+import knex from './dataBase/connection';
 
 const routes = express.Router()
 
 routes.get('/', (request, response) => {
-    response.json({message: 'server on'})
+    response.json({ message: 'server on' })
 })
+
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        };
+    });
+
+    return response.json(serializedItems)
+})
+
+
+routes.get('/points', async (request, response) => {
+    const points = await knex('points').select('*');
+    return response.json(points)
+})
+
 export default routes;
