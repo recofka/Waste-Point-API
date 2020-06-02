1ceae8b07fa9dc836a415f7ffa303460cc82d329
recofka <recofka@gmail.com>
2020-06-02 21:04:21 +0200

    Created the migrations files to all database tables

--- src/dataBase/migrations/00_create_points.ts
+++ src/dataBase/migrations/00_create_points.ts
@@ -1,7 +1,7 @@
 import Knex from 'knex';
 
 export async function up(knex: Knex){
-    knex.schema.createTable('points', table => {
+    return knex.schema.createTable('points', table => {
         table.increments('id').primary();
         table.string('image').notNullable();
         table.string('name').notNullable();
@@ -13,5 +13,5 @@ export async function up(knex: Knex){
 }
 
 export async function down(knex: Knex){
-    return knex.schema.dropTable('points');
+    return knex.schema.dropTable('points'); 
 }
